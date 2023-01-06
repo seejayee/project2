a0457d1273c8bf54a45fc2b8373717bd9e99f3ac
Ben Fifield <actuallysmart@yahoo.com>
2023-01-05 20:01:10 -0800

    search functionality

--- controllers/api/mediaroutes.js
+++ controllers/api/mediaroutes.js
@@ -7,42 +7,71 @@ const SpotifyWebApi = require('spotify-web-api-node');
 const withToken = require('../../utils/spotToken');
 const spotifyApi = new SpotifyWebApi(withToken._credentials);
 
-router.get('/', withAuth, async (req, res) => {
+router.get('/songs/:title', withAuth, async (req, res) => {
   console.log('\n\nsearching for music\n\n');
 
-      // These console.log functions are various ways to access useful information via the spotify-web-api-node package.
+  // These console.log functions are various ways to access useful information via the spotify-web-api-node package.
+
+  spotifyApi.searchTracks(req.params.title).then(
+    function (data) {
+      console.log(data);
+      res.render('searchResults', {
+        data
+      })
+      return 
       console.log(
-      spotifyApi.searchTracks('Come Together')
-  .then(function(data) {
-    console.log('Here is the track name of the first search result: ', JSON.stringify(data.body.tracks.items[0].name));
-    console.log('Here is the URL for the first search result: ', JSON.stringify(data.body.tracks.items[0].external_urls.spotify));
-    console.log('Here is the Spotify ID for the first search result: ', JSON.stringify(data.body.tracks.items[0].id));
-    console.log('Here is the first artist listed for the first search result: ', JSON.stringify(data.body.tracks.items[0].artists[0].name));
-    console.log('Here is the album name for the first search result: ', JSON.stringify(data.body.tracks.items[0].album.name), "\n\n---------------------\n");
-    // console.log(data.body.tracks.items.map(ele => ele.name));
+        'Here is the track name of the first search result: ',
+        JSON.stringify(data.body.tracks.items[0].name)
+      );
+      console.log(
+        'Here is the URL for the first search result: ',
+        JSON.stringify(data.body.tracks.items[0].external_urls.spotify)
+      );
+      console.log(
+        'Here is the Spotify ID for the first search result: ',
+        JSON.stringify(data.body.tracks.items[0].id)
+      );
+      console.log(
+        'Here is the first artist listed for the first search result: ',
+        JSON.stringify(data.body.tracks.items[0].artists[0].name)
+      );
+      console.log(
+        'Here is the album name for the first search result: ',
+        JSON.stringify(data.body.tracks.items[0].album.name),
+        '\n\n---------------------\n'
+      );
+      // console.log(data.body.tracks.items.map(ele => ele.name));
 
-    songResults = data.body.tracks.items.map(ele => ele.name);
+      songResults = data.body.tracks.items.map((ele) => ele.name);
 
-    artistResults = data.body.tracks.items.map(ele => ele.artists.map(ele => ele.name));
+      artistResults = data.body.tracks.items.map((ele) =>
+        ele.artists.map((ele) => ele.name)
+      );
 
-    console.log(songResults.map((e, i) => [e, artistResults[i]]));
+      console.log(songResults.map((e, i) => [e, artistResults[i]]));
 
-    console.log("\n", 
-      data.body.tracks.items.forEach(element => {
-        console.log("\n\n", element.name, ",\nby artist(s): ", element.artists
-          .forEach(element => {
-          console.log(element.name)
-        }
-        ));
-      }), "\n\n"
-    );
-    // console.log('Here is all the data for the first search result: ', JSON.stringify(data.body.tracks.items[0]));
-    console.log("Here I am")
-
-  }, function(err) {
-    console.error(err);
-  })
-    );
+      console.log(
+        '\n',
+        data.body.tracks.items.forEach((element) => {
+          console.log(
+            '\n\n',
+            element.name,
+            ',\nby artist(s): ',
+            element.artists.forEach((element) => {
+              console.log(element.name);
+            })
+          );
+        }),
+        '\n\n'
+      );
+      // console.log('Here is all the data for the first search result: ', JSON.stringify(data.body.tracks.items[0]));
+      console.log('Here I am');
+    },
+    function (err) {
+      console.error(err);
+    }
+  );
+ 
 
 });
 
@@ -52,8 +81,6 @@ router.post('/', withAuth, async (req, res) => {
       ...req.body,
       user_id: req.session.user_id,
     });
-
-
 
     res.status(200).json(newMedia);
   } catch (err) {
